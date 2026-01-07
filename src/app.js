7c823e134a4ed46cd422efac6265ea0394dd1ba8
Aarish <a.aarish312@gmail.com>
2026-01-07 17:44:17 +0530

    eslint & preetier

--- src/app.js
+++ src/app.js
@@ -2,10 +2,10 @@ import express from 'express';
 
 const app = express();
 
-const PORT = process.env.PORT||3000;
+const PORT = process.env.PORT || 3000;
 
-app.get('/',(req,res)=>{
-    res.status(200).send('hello from the acquisition')
-})
+app.get('/', (req, res) => {
+  res.status(200).send('hello from the acquisition');
+});
 
 export default app;
